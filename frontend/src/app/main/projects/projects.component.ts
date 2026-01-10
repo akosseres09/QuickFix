81fc2d50faecd5554bfbe428f183d2e0b42e0edb
akosseres09 <akosseres75@gmail.com>
2026-01-10 10:17:47 +0100

    feat: added project name filter option to projects page

--- frontend/src/app/main/projects/projects.component.ts
+++ frontend/src/app/main/projects/projects.component.ts
@@ -5,19 +5,56 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { TableComponent } from '../../common/table/table.component';
 import { Project } from '../../shared/model/Project';
 import { DisplayedColumn } from '../../shared/constants/DisplayedColumn';
-import { ProjectService } from '../../shared/services/project/project.service';
+import { ProjectFilters, ProjectService } from '../../shared/services/project/project.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import {
+    MAT_FORM_FIELD_DEFAULT_OPTIONS,
+    MatFormField,
+    MatLabel,
+} from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
+import { MatInput } from '@angular/material/input';
+import { debounce } from 'rxjs';
+import { UrlService } from '../../shared/services/url/url.service';
+import { ActivatedRoute } from '@angular/router';
+import { MatButton } from '@angular/material/button';
 
 @Component({
     selector: 'app-projects',
-    imports: [MatTableModule, MatPaginatorModule, CommonModule, TableComponent],
+    imports: [
+        MatTableModule,
+        MatPaginatorModule,
+        CommonModule,
+        TableComponent,
+        ReactiveFormsModule,
+        MatAutocompleteModule,
+        MatOptionModule,
+        MatFormField,
+        MatInput,
+        MatLabel,
+        MatButton,
+    ],
+    providers: [
+        { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } },
+    ],
     templateUrl: './projects.component.html',
     styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
-    private projectService = inject(ProjectService);
+    private readonly urlService = inject(UrlService);
+    private readonly fb = inject(FormBuilder);
+    private readonly projectService = inject(ProjectService);
+    private readonly activeRoute = inject(ActivatedRoute);
     projects = signal<Project[]>(this.projectService.getProjects());
-    shownProjects = computed(() => new MatTableDataSource<Project>(this.projects()));
-    displayedColumns: Array<DisplayedColumn> = [
+
+    filterForm = this.fb.group({
+        projectName: [this.activeRoute.snapshot.queryParamMap.get('projectName') || ''],
+    });
+    showFilterReset = signal<boolean>(this.activeRoute.snapshot.queryParamMap.has('projectName'));
+    filteredProjects = signal<Project[]>(this.projectService.getProjects(this.filterForm.value));
+    shownProjects = computed(() => new MatTableDataSource<Project>(this.filteredProjects()));
+    displayedColumns = signal<DisplayedColumn[]>([
         {
             id: 'name',
             label: 'Name',
@@ -43,5 +80,29 @@ export class ProjectsComponent {
             sortable: true,
             value: (e: Project) => e.createdAt,
         },
-    ];
+    ]);
+
+    constructor() {
+        this.filterForm.valueChanges
+            .pipe(debounce(() => new Promise((resolve) => setTimeout(resolve, 300))))
+            .subscribe((filters) => {
+                const filterArray = Object.entries(filters);
+                filterArray.forEach(([key, value]) => {
+                    if (!value) {
+                        filters[key as keyof ProjectFilters] = null;
+                    }
+                });
+
+                if (filterArray.some(([_, value]) => value)) {
+                    this.showFilterReset.set(true);
+                }
+                this.urlService.addQueryParams(filters);
+                this.filteredProjects.set(this.projectService.getProjects(filters));
+            });
+    }
+
+    resetFilters() {
+        this.filterForm.reset();
+        this.showFilterReset.set(false);
+    }
 }
--- frontend/src/app/shared/services/project/project.service.ts
+++ frontend/src/app/shared/services/project/project.service.ts
@@ -3,14 +3,18 @@ import { Project } from '../../model/Project';
 import { UserService } from '../user/user.service';
 import { User } from '../../model/User';
 
+export interface ProjectFilters {
+    projectName?: string | null;
+}
+
 @Injectable({
     providedIn: 'root',
 })
 export class ProjectService {
     constructor(private userService: UserService) {}
 
-    getProjects(): Array<Project> {
-        return [
+    getProjects(filters: Partial<ProjectFilters> = {}): Array<Project> {
+        let baseProjects = [
             {
                 id: '1',
                 name: 'Google IT',
@@ -33,5 +37,14 @@ export class ProjectService {
                 issues: [],
             },
         ];
+
+        baseProjects = baseProjects.filter((project) => {
+            if (filters.projectName) {
+                return project.name.toLowerCase().includes(filters.projectName.toLowerCase());
+            }
+            return true;
+        });
+
+        return baseProjects;
     }
 }
