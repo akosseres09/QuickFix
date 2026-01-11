6afced30cd5c82b61e04c5b05b99d7040fe0c4e5
akosseres09 <akosseres75@gmail.com>
2026-01-11 23:42:44 +0100

    feat: fixing navbar ui and mobile menu close on navigation

--- frontend/src/app/common/navbar/navbar.component.ts
+++ frontend/src/app/common/navbar/navbar.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, inject, input, OnInit, output, signal } from '@angular/core';
+import { AfterViewInit, Component, inject, input, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
 import { MatIcon } from '@angular/material/icon';
-import { Router, RouterLink, RouterModule } from '@angular/router';
+import { Event, NavigationStart, Router, RouterLink, RouterModule } from '@angular/router';
 import { ThemeService } from '../../shared/services/theme/theme.service';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { AppRoute } from '../../shared/constants/Routes';
@@ -11,7 +11,7 @@ import { RouteService } from '../../shared/services/route/route.service';
 import { User } from '../../shared/model/User';
 import { SidebarService } from '../../shared/services/sidebar/sidebar.service';
 import { UserService } from '../../shared/services/user/user.service';
-import { fromEvent, take } from 'rxjs';
+import { filter, fromEvent, take } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
@@ -30,33 +30,28 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
     styleUrl: './navbar.component.css',
     standalone: true,
 })
-export class NavbarComponent implements OnInit, AfterViewInit {
-    showSidebarToggle = input<boolean>(true);
-    sidebarClosed = output<boolean>();
-    isSidebarCollapsed = signal<boolean>(false);
-    isMenuOpen = signal<boolean>(false);
-    imageSource: string = 'QuickFix_logo_dark.png';
-    user = signal<User | null>(null);
-    htmlElement = signal<HTMLElement | null>(null);
-    routes = signal<AppRoute[]>([]);
-    logo;
-
+export class NavbarComponent implements AfterViewInit {
     private themeService = inject(ThemeService);
     private router = inject(Router);
     private routeService = inject(RouteService);
     private sidebarService = inject(SidebarService);
     private userService = inject(UserService);
 
+    showSidebarToggle = input<boolean>(true);
+    sidebarClosed = output<boolean>();
+    isSidebarCollapsed = signal<boolean>(this.sidebarService.getState());
+    isMenuOpen = signal<boolean>(false);
+    imageSource: string = 'QuickFix_logo_dark.png';
+    user = signal<User | null>(this.userService.getUser());
+    htmlElement = signal<HTMLElement | null>(document.documentElement);
+    routes = signal<AppRoute[]>(
+        this.routeService.getAppRoutes(this.user()).filter((route) => route.show)
+    );
+    logo = this.themeService.logos;
     theme = signal<'light' | 'dark'>(this.themeService.getTheme() || 'light');
 
-    ngOnInit(): void {
-        this.user.set(this.userService.getUser());
-        this.htmlElement.set(document.documentElement);
-        this.setTheme(this.theme());
-        this.routes.set(this.routeService.getAppRoutes(this.user()).filter((route) => route.show));
-    }
-
     constructor() {
+        this.setTheme(this.theme());
         fromEvent(window, 'resize')
             .pipe(takeUntilDestroyed())
             .subscribe(() => {
@@ -65,8 +60,16 @@ export class NavbarComponent implements OnInit, AfterViewInit {
                 }
             });
 
-        this.isSidebarCollapsed.set(this.sidebarService.getState());
-        this.logo = this.themeService.logos;
+        this.router.events
+            .pipe(
+                filter(
+                    (event: Event): event is NavigationStart => event instanceof NavigationStart
+                ),
+                takeUntilDestroyed()
+            )
+            .subscribe(() => {
+                this.isMenuOpen.set(false);
+            });
     }
 
     ngAfterViewInit(): void {
@@ -96,10 +99,6 @@ export class NavbarComponent implements OnInit, AfterViewInit {
         }
 
         this.setTheme(event.value);
-    }
-
-    navigateTo(path: string) {
-        this.router.navigateByUrl(path);
     }
 
     toggleSidebar() {
