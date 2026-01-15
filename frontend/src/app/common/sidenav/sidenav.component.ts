e3fec70321c927044b6308074412e4b4e9da7328
akosseres09 <akosseres75@gmail.com>
2026-01-15 10:16:14 +0100

    feat: sidenav

--- frontend/src/app/common/sidenav/sidenav.component.ts
+++ frontend/src/app/common/sidenav/sidenav.component.ts
@@ -1,12 +1,13 @@
-import { Component, inject, model, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, inject, model, signal } from '@angular/core';
 import { AppRoute } from '../../shared/constants/Routes';
 import { ThemeService } from '../../shared/services/theme/theme.service';
 import { RouteService } from '../../shared/services/route/route.service';
 import { SidebarService } from '../../shared/services/sidebar/sidebar.service';
 import { NavitemComponent } from './navitem/navitem.component';
 import { NavigationEnd, Router } from '@angular/router';
-import { filter, Subscription } from 'rxjs';
+import { filter } from 'rxjs';
 import { matchProjectRoutes } from '../../shared/constants/RouteMatch';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-sidenav',
@@ -14,37 +15,29 @@ import { matchProjectRoutes } from '../../shared/constants/RouteMatch';
     templateUrl: './sidenav.component.html',
     styleUrl: './sidenav.component.css',
 })
-export class SidenavComponent implements OnInit, OnDestroy {
-    isCollapsed = model<boolean>(false);
-    topRoutes = model<AppRoute[]>([]);
-    bottomRoutes = signal<AppRoute[]>([]);
-    logo = signal<string>('');
-    theme = signal<'light' | 'dark'>('light');
-
+export class SidenavComponent {
     private themeService = inject(ThemeService);
     private routeService = inject(RouteService);
     private sidebarService = inject(SidebarService);
     private router = inject(Router);
-    private navigationSubscription: Subscription | null = null;
 
-    ngOnInit(): void {
-        this.navigationSubscription = this.router.events
-            .pipe(filter((event) => event instanceof NavigationEnd))
+    isCollapsed = model<boolean>(this.sidebarService.getState());
+    topRoutes = model<AppRoute[]>(this.routeService.getSidenavRoutes());
+    bottomRoutes = signal<AppRoute[]>(this.routeService.getBottomSidenavRoutes());
+    theme = signal<'light' | 'dark'>(this.themeService.getTheme());
+    logo = signal<string>(this.themeService.logos[this.theme()]);
+
+    constructor() {
+        this.router.events
+            .pipe(
+                filter((event) => event instanceof NavigationEnd),
+                takeUntilDestroyed()
+            )
             .subscribe((event: NavigationEnd) => {
                 if (event.url.match(matchProjectRoutes)) {
                     this.topRoutes.set(this.routeService.getSidenavRoutes());
                 }
             });
-
-        this.theme.set(this.themeService.getTheme());
-        this.isCollapsed.set(this.sidebarService.getState());
-        this.topRoutes.set(this.routeService.getSidenavRoutes());
-        this.bottomRoutes.set(this.routeService.getBottomSidenavRoutes());
-        this.logo.set(this.themeService.logos[this.theme()]);
-    }
-
-    ngOnDestroy(): void {
-        this.navigationSubscription?.unsubscribe();
     }
 
     toggleSidebar(): void {
