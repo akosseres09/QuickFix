33f8b72b50883c8a2357cf90c13d75bae2a5d902
akosseres09 <akosseres75@gmail.com>
2026-03-05 15:10:32 +0100

    feat: renaming components

---
+++ frontend/src/app/main/organizations/create-organization/create-organization.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-create',
+    imports: [],
+    templateUrl: './create-organization.component.html',
+    styleUrl: './create-organization.component.css',
+})
+export class CreateOrganizationComponent {}
---
+++ frontend/src/app/main/organizations/edit-organization/edit-organization.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-edit',
+    imports: [],
+    templateUrl: './edit-organization.component.html',
+    styleUrl: './edit-organization.component.css',
+})
+export class EditOrganizationComponent {}
