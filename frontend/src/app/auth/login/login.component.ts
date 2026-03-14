b1fd35a4aec6febb01fdc37b8bcea534897a5b9c
akosseres09 <akosseres75@gmail.com>
2026-03-14 13:01:22 +0100

    feat: fix base layout and auth page UI

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, inject, signal } from '@angular/core';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatAnchor } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import {
     MatError,
@@ -30,6 +30,7 @@ import { SnackbarService } from '../../shared/services/snackbar/snackbar.service
         RouterLink,
         ReactiveFormsModule,
         CommonModule,
+        MatAnchor,
     ],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
