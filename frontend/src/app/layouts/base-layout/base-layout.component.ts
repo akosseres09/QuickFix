c42bfaaca1cc12f71cdaa4f8d41ea0e6a8074ec9
akosseres09 <akosseres75@gmail.com>
2026-03-30 21:32:22 +0200

    feat: remove unnecessary imports, attributes and functions from base layout

--- frontend/src/app/layouts/base-layout/base-layout.component.ts
+++ frontend/src/app/layouts/base-layout/base-layout.component.ts
@@ -1,10 +1,8 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavbarComponent } from '../../common/navbar/navbar.component';
-import { User } from '../../shared/model/User';
-import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { filter } from 'rxjs';
 import { FooterComponent } from '../../common/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-base',
@@ -12,26 +10,4 @@ import { CommonModule } from '@angular/common';
     templateUrl: './base-layout.component.html',
     styleUrl: './base-layout.component.css',
 })
-export class BaseLayoutComponent implements OnInit {
-    user = signal<User | null>(null);
-    innerPage = signal<boolean>(false);
-    router = inject(Router);
-    activeRoute = inject(ActivatedRoute);
-
-    ngOnInit(): void {
-        this.checkPage(this.router.url);
-        this.router.events
-            .pipe(filter((event) => event instanceof NavigationEnd))
-            .subscribe((event) => {
-                this.checkPage(event.url);
-            });
-    }
-
-    checkPage(url: string) {
-        if (url.match('/(organizations|worktime|account|settings)')) {
-            this.innerPage.set(true);
-        } else {
-            this.innerPage.set(false);
-        }
-    }
-}
+export class BaseLayoutComponent {}
